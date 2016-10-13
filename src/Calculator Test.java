1295fca902959fa10fd9521fbf7b3799d0587995
orlygur <orlygurolafs@gmail.com>
2016-10-13 15:47:48 +0000

    Done with kata steps 1 and 2

--- src/Calculator Test.java
+++ src/Calculator Test.java
@@ -4,5 +4,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.Add(""));
+	}
 	
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ package String-Calculator;
 
 public class Calculator {
 	public static int Add( String num ) {
-		if ( num == "" ) return 0;
-		return -1;
+		if ( num.equals("") return 0;
+		return 1;
 	}
 }
