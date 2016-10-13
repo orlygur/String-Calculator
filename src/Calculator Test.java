0b98b3b9f8948dcdb97e0b7781aa244d436e698d
orlygur <orlygurolafs@gmail.com>
2016-10-13 16:17:00 +0000

    lidur eitt buinn

--- src/Calculator Test.java
+++ src/Calculator Test.java
@@ -11,6 +11,21 @@ public class CalculatorTest {
 	
 	@Test
 	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
+		assertEquals(1, Calculator.Add("1"));
+	}
+	
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.Add("1,2"));
+	}
+	
+	@Test
+	public void testThreeNumbers() {
+		assertEquals(6, Calculator.Add("1,2,3"));
+	}
+	
+	@Test
+	public void testFourNumbers() {
+		assertEquals(10, Calculator.Add("1,2,3,4"));
 	}
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,14 @@ package String-Calculator;
 
 public class Calculator {
 	public static int Add( String num ) {
-		if ( num.equals("") return 0;
+		
+		if ( num.equals( "" ) return 0;
+		
+		else if ( num.contains(",") ) return sum(splitNumbers(text));
+		
 		return 1;
+	}
+	private static int ToInt( String number ){
+		return Integer.parseInt( number );
 	}
 }
